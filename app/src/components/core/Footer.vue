34ff5a2f0f0c9178277cc385b5573d2cfd316338
Tundzhay Mollov <graculapsone1998@gmail.com>
2020-03-31 13:52:56 +0300

    Created Footer component

---
+++ app/src/components/core/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer class="p-1 bg-dark fixed-bottom">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright © $$ Donation University $$ 2019</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "app-footer"
+};
+</script>
+
+<style scoped>
+</style>
