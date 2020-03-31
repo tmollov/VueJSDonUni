41412d5939355356de1d797db8ea49f1a084bb53
Tundzhay Mollov <graculapsone1998@gmail.com>
2020-03-31 13:53:56 +0300

    Created Home/Guest component

---
+++ app/src/components/home/Guest.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container mainDiv">
+    <div class="row align-items-center my-5">
+      <div class="col-lg-12">
+        <img
+          class="img-fluid rounded mb-4 mb-lg-0"
+          src="https://www.firstmid.com/wp-content/uploads/OpenforBusiness_SBALoan-900x400.png"
+          alt="HomeGuestPageImage"
+        />
+      </div>
+      <div class="col-lg-12 supportDiv">
+        <h1 class="font-weight-light">Does your business need support?</h1>
+
+        <p><a class="badge badge-primary" href>Register now</a> to create your cause and seek for donors!</p>
+        
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app-home-guest"
+};
+</script>
+
+<style scoped>
+.mainDiv {
+  margin-top: 3%;
+  padding-top: 3%;
+}
+
+img {
+  margin: 0 auto;
+  display: block;
+}
+
+.supportDiv {
+  text-align: center;
+  margin-top: 5%;
+}
+</style> 
