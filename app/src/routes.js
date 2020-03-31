920e6f43a28aa51d5d186090a2a12d854828112c
Tundzhay Mollov <graculapsone1998@gmail.com>
2020-03-31 21:26:29 +0300

    Added /cause/create path to routes

--- app/src/routes.js
+++ app/src/routes.js
@@ -1,9 +1,11 @@
-import HomeGuest from './components/home/Guest.vue';
+import HomeMain from './components/home/Main.vue';
 import Login from './components/auth/Login.vue';
 import Register from './components/auth/Register.vue';
+import CreateCause from './components/cause/Create.vue';
 
 export default [
-    { path: '/', component: HomeGuest },
+    { path: '/', component: HomeMain },
     { path: '/login', component: Login },
     { path: '/register', component: Register },
+    { path: '/cause/create', component: CreateCause },
 ]
