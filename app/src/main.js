7af5b4d39aba804803dcf2fbaa1052da2e89596c
Tundzhay Mollov <graculapsone1998@gmail.com>
2020-03-31 21:25:11 +0300

    Update main.js
    Added globally fontawesome component

--- app/src/main.js
+++ app/src/main.js
@@ -3,7 +3,18 @@ import VueRouter from 'vue-router';
 import App from "./App.vue";
 import routes from "./routes";
 import store from "./store/store"
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faDonate, faPencilAlt } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+
+library.add(faDonate)
+library.add(faPencilAlt);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false;
+
 
 Vue.use(VueRouter);
 
