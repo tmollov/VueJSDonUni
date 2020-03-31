30d9c569395034306848d50673da7bea36687e53
Tundzhay Mollov <graculapsone1998@gmail.com>
2020-03-31 20:02:01 +0300

    Added vuex and vue-router to the app main.js

--- app/src/main.js
+++ app/src/main.js
@@ -1,8 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import VueRouter from 'vue-router';
+import App from "./App.vue";
+import routes from "./routes";
+import store from "./store/store"
+Vue.config.productionTip = false;
 
-Vue.config.productionTip = false
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode:"history",
+  routes
+})
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+  router,
+  store
+}).$mount("#app");
