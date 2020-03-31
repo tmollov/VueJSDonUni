2602bff4a547899a4a14bee2bd59f057aada152e
Tundzhay Mollov <graculapsone1998@gmail.com>
2020-03-31 21:25:54 +0300

    Update store.js
    Now Vuex uses vuex-persistedstate plugin

--- app/src/store/store.js
+++ app/src/store/store.js
@@ -1,16 +1,22 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
+
 Vue.use(Vuex);
+
 export default new Vuex.Store({
   state: {
     IsUserLogged: false
   },
   mutations: {
-    logUser(state, IsUserLogged) {
+    changeLogState(state, IsUserLogged) {
       state.IsUserLogged = IsUserLogged;
     }
   },
   getters: {
     IsUserLogged: state => state.IsUserLogged
-  }
+  },
+  plugins:[createPersistedState({
+    paths: ['IsUserLogged']
+  })]
 });
