152a83c751f54a4b18295d90487cf9a2eea56978
Tundzhay Mollov <graculapsone1998@gmail.com>
2020-04-01 20:39:29 +0300

    Update Login.vue
    Added:
    - firebase singIn
    - loader while singin in

--- app/src/components/auth/Login.vue
+++ app/src/components/auth/Login.vue
@@ -1,20 +1,30 @@
 <template>
   <div class="modal-content col-lg-4 loginDiv">
     <div class="card-body">
-      <form >
+      <form>
         <p class="h4 py-4">Login</p>
         <hr />
-        <label for="email" class="grey-text font-weight-light w-100 text-left font-weight-bold">Username</label>
-        <input id="email" type="email" class="form-control" />
+        <label
+          for="email"
+          class="grey-text font-weight-light w-100 text-left font-weight-bold"
+        >Username</label>
+        <input id="email" type="email" class="form-control" v-model="email" />
 
         <br />
 
-        <label for="password" class="grey-text font-weight-light w-100 text-left font-weight-bold">Password</label>
-        <input id="password" type="password" class="form-control" />
+        <label
+          for="password"
+          class="grey-text font-weight-light w-100 text-left font-weight-bold"
+        >Password</label>
+        <input id="password" type="password" class="form-control" v-model="password" />
 
         <div class="text-center py-4 mt-3">
-          <button class="btn btn-info" @click.prevent="login">Log In</button>
-          <p>Don't have an account? <router-link to="/register">Register now!</router-link></p>
+          <button v-if="!loading" class="btn btn-info" @click.prevent="login">Log In</button>
+          <app-loader-ring v-if="loading" :width="20" :height="20"></app-loader-ring>
+          <p>
+            Don't have an account?
+            <router-link to="/register">Register now!</router-link>
+          </p>
         </div>
       </form>
     </div>
@@ -22,14 +32,45 @@
 </template>
 
 <script>
+import firebase from "firebase";
+import AppLoaderRing from "../core/PureRingLoader";
+
 export default {
   name: "app-login",
-  methods:{
-    login(){
-      this.$store.commit("changeLogState",true);
-      this.$router.push("/");
+  components: {
+    AppLoaderRing
+  },
+  data() {
+    return {
+      email: "",
+      password: "",
+      loading: false
+    };
+  },
+  methods: {
+    login() {
+      this.loading = true;
+
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then(user => {
+          console.log(user);
+          this.$store.commit("changeUserState", user.user);
+          this.loading = false;
+          this.$router.push("/")
+        })
+        .catch(err => {
+          console.log(err);
+          this.loading = false;
+        });
     }
-  }
+  },
+  created() {
+    if (this.$store.getters.User) {
+      this.$router.push("/")
+    }
+  },
 };
 </script>
 
