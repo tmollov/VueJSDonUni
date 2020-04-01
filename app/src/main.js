e17c0f32a6f81c6d7aa9f058741c64ed3d9b6902
Tundzhay Mollov <graculapsone1998@gmail.com>
2020-04-01 17:03:06 +0300

    Update main.js
    New fontawesome icon: briefcase

--- app/src/main.js
+++ app/src/main.js
@@ -1,19 +1,25 @@
 import Vue from "vue";
-import VueRouter from 'vue-router';
+import VueRouter from "vue-router";
 import App from "./App.vue";
 import routes from "./routes";
-import store from "./store/store"
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faDonate, faPencilAlt, faSearchDollar } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import firebase from 'firebase';
-import firebaseConfig from 'firebaseConfig';
+import store from "./store/store";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faDonate,
+  faPencilAlt,
+  faSearchDollar,
+  faBriefcase
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import firebase from "firebase";
+import firebaseConfig from "./firebaseConfig";
 
-library.add(faDonate)
+library.add(faDonate);
 library.add(faPencilAlt);
-library.add(faSearchDollar)
+library.add(faSearchDollar);
+library.add(faBriefcase);
 
-Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 firebase.initializeApp(firebaseConfig);
 Vue.config.productionTip = false;
@@ -21,9 +27,9 @@ Vue.config.productionTip = false;
 Vue.use(VueRouter);
 
 const router = new VueRouter({
-  mode:"history",
+  mode: "history",
   routes
-})
+});
 
 new Vue({
   render: h => h(App),
