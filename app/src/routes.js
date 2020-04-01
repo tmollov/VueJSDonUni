e9f919d6d9456fa7c1e1ecc8fcd0332e970e12e2
Tundzhay Mollov <graculapsone1998@gmail.com>
2020-04-01 17:04:18 +0300

    Update routes.js

--- app/src/routes.js
+++ app/src/routes.js
@@ -1,17 +1,17 @@
-import HomeMain from './components/home/Main.vue';
-import HomeDashbord from './components/home/Dashboard.vue'
-import Login from './components/auth/Login.vue';
-import Register from './components/auth/Register.vue';
-import CauseCreate from './components/cause/Create.vue';
-import CauseDetail from './components/cause/Detail.vue';
-import NotFound from './components/core/NotFound.vue'
+import HomeMain from "./components/home/Main.vue";
+import HomeDashbord from "./components/home/Dashboard.vue";
+import Login from "./components/auth/Login.vue";
+import Register from "./components/auth/Register.vue";
+import CauseCreate from "./components/cause/Create.vue";
+import CauseDetail from "./components/cause/Detail.vue";
+import NotFound from "./components/core/NotFound.vue";
 
 export default [
-    { path: '/', component: HomeMain },
-    { path: '/dashboard', component: HomeDashbord },
-    { path: '/login', component: Login },
-    { path: '/register', component: Register },
-    { path: '/cause/create', component: CauseCreate },
-    { path: '/cause/detail', component: CauseDetail },
-    { path: '*', component: NotFound}
-]
+  { path: "/", component: HomeMain },
+  { path: "/dashboard", component: HomeDashbord },
+  { path: "/login", component: Login },
+  { path: "/register", component: Register },
+  { path: "/cause/create", component: CauseCreate },
+  { path: "/cause/detail", component: CauseDetail },
+  { path: "*", component: NotFound }
+];
