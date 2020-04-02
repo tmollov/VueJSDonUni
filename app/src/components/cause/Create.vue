a9e5a85ec345a75db58f047cf6051a480d8e98c2
Tundzhay Mollov <graculapsone1998@gmail.com>
2020-04-03 00:33:53 +0300

    Update Cause/Create.vue
    Added:
    - two-way binding for inputs
    - imported uuid for generating id for new cause
    - sendData method that adds cause to the database
    - css for desciption textarea

--- app/src/components/cause/Create.vue
+++ app/src/components/cause/Create.vue
@@ -2,49 +2,48 @@
   <div class="modal-content col-lg-6 mx-auto mt-lg-5">
     <div class="modal-header info-color white-text">
       <h4 class="title">
-        <font-awesome-icon icon="search-dollar" /> You are looking for a donation?
+        <font-awesome-icon icon="search-dollar" />You are looking for a donation?
       </h4>
     </div>
     <div class="modal-body">
-      <form >
-        <label for="defaultFormNameModalEx">Your cause</label>
+      <form>
+        <label for="causeName">Your cause</label>
         <input
-          name="cause"
           type="text"
-          id="defaultFormNameModalEx"
+          id="causeName"
           class="form-control form-control-sm"
-          placeholder="Collage..."
+          placeholder="Cause title..."
+          v-model="title"
         />
         <br />
-        <label for="defaultFormEmailModalEx">Your cause picture...</label>
+        <label for="pictureUrl">Your cause picture...</label>
         <input
-          name="pictureUrl"
           type="text"
-          id="defaultFormEmailModalEx"
+          id="pictureUrl"
           class="form-control form-control-sm"
           placeholder="https://..."
+          v-model="image"
         />
         <br />
-        <label for="defaultFormSubjectModalEx">Your needed funds...</label>
+        <label for="neededFunds">Your needed funds...</label>
         <input
-          name="neededFunds"
           type="text"
-          id="defaultFormSubjectModalEx"
+          id="neededFunds"
           class="form-control form-control-sm"
           placeholder="2400.00"
+          v-model="neededFund"
         />
         <br />
-        <label for="defaultFormMessageModalEx">Your cause description...</label>
+        <label for="causeDescription">Your cause description...</label>
         <textarea
-          name="description"
-          style="resize: none; height: 150px;"
           type="text"
-          id="defaultFormMessageModalEx"
+          id="causeDescription"
           class="md-textarea form-control"
           placeholder="I'm looking for funds because..."
+          v-model="description"
         ></textarea>
         <div class="text-center mt-4 mb-2">
-          <button @click.prevent="showlog()" class="btn btn-info">Send</button>
+          <button @click.prevent="sendData()" class="btn btn-info">Send</button>
         </div>
       </form>
     </div>
@@ -52,12 +51,42 @@
 </template>
 
 <script>
+import firebase from "firebase";
+import { v4 as uuidv4 } from "uuid";
+import { Cause } from "../../models/Cause";
+
 export default {
   name: "app-cause-create",
-  methods:{
-    showlog(){
-      console.log(this.$store);
-      
+  data() {
+    return {
+      title: "",
+      image: "",
+      description: "",
+      neededFund: 0
+    };
+  },
+  methods: {
+    sendData() {
+      let id = uuidv4();
+      let newCause = new Cause(
+        this.title,
+        this.description,
+        this.image,
+        this.neededFund,
+        this.$store.getters.User.email
+      );
+      console.log(newCause);
+
+      firebase
+        .database()
+        .ref()
+        .child("causes")
+        .child(id)
+        .set(newCause)
+        .then((res)=>{
+          console.log(res);
+          this.$router.push("/")
+        });
     }
   }
 };
@@ -68,5 +97,10 @@ export default {
   margin: 0 auto;
   margin-bottom: 7%;
   margin-top: 7%;
+}
+
+textarea {
+  resize: none;
+  height: 150px;
 }
 </style>
