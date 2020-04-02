1c0c0edfa5704b071ebe65a88583265cac93749a
Tundzhay Mollov <graculapsone1998@gmail.com>
2020-04-03 00:38:59 +0300

    Update Cause/Detail.vue
    Added:
    - transitions
    - deleteCause method to delete current cause
    - loading cause details in mounted

--- app/src/components/cause/Detail.vue
+++ app/src/components/cause/Detail.vue
@@ -1,61 +1,121 @@
 <template>
-  <div class="container detailDiv mb-5">
-    <div class="row">
-      <div class="modal-content col-lg-12">
-        <!-- <div class="modal-header">
+  <div>
+    <transition name="fadeOut">
+      <app-loader v-if="loading" class="loadingRing"></app-loader>
+    </transition>
+
+    <transition name="fadeIn">
+      <div v-if="!loading" class="container detailDiv mb-5">
+        <div class="row">
+          <div class="modal-content col-lg-12">
+            <!-- <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
           <h4 class="modal-title" id="myModalLabel">Make a donation</h4>
-        </div> -->
-        <div class="modal-body">
-          <center>
-            <img
-              style="border-radius: 5px;"
-              src="https://cdn.pixabay.com/photo/2017/01/20/00/30/maldives-1993704_960_720.jpg"
-              name="aboutme"
-              width="500"
-              height="auto"
-              border="0"
-              class="img-circle"
-            />
-            <h3 class="media-heading">CAUSE</h3>
-            <span>Collected </span>
-            <strong class="label text-success">$0.00</strong>
-            <span> from </span>
-            <strong class="label text-info"> $900000.00</strong>
-          </center>
-          <hr />
-          <center>
-            <p style="word-wrap: break-word;" class="text-left">
-              <strong>Description:</strong>
-              <br />DESCRIPTION
-            </p>
-            <br />
-            <p style="word-wrap: break-word;" class="text-left">
-              <strong>Donors:</strong>
-              <br />
-              <span>Donor 1</span>
-              <span>Donor 2</span>
-              <span>Donor 3</span>
-              <span>Donor 4</span>
-            </p>
-          </center>
-        </div>
-        <div class="modal-footer">
-          <a href class="btn btn-danger">Close the cause</a>
-          <form>
-            <label for="currentDonation" class="grey-text font-weight-light">My donation:</label>
-            <input id="currentDonation" name="currentDonation" type="number" min="1" class="form-control" />
-            <button style="width: 100%;" class="btn btn-success" type="submit">Make the donation</button>
-          </form>
+            </div>-->
+            <div class="modal-body">
+              <center>
+                <img
+                  style="border-radius: 5px;"
+                  :src="getImage"
+                  name="aboutme"
+                  width="500"
+                  height="auto"
+                  border="0"
+                  class="img-circle"
+                />
+                <h3 class="media-heading">{{cause.title}}</h3>
+                <span>Collected</span>
+                <strong class="label text-success">{{cause.neededFund}}</strong>
+                <span>from</span>
+                <strong class="label text-info">{{cause.collectedFund}}</strong>
+              </center>
+              <hr />
+              <center>
+                <p style="word-wrap: break-word;" class="text-left">
+                  <strong>Description:</strong>
+                  <br />
+                  {{cause.description}}
+                </p>
+                <br />
+                <p style="word-wrap: break-word;" class="text-left">
+                  <strong>Donors:</strong>
+                  <br />
+                  <span>Donor 1</span>
+                  <span>Donor 2</span>
+                  <span>Donor 3</span>
+                  <span>Donor 4</span>
+                </p>
+              </center>
+            </div>
+            <div class="modal-footer">
+              <a
+                v-if="$store.getters.User"
+                href="#deletecause"
+                class="btn btn-danger"
+                @click="deleteCause()"
+              >Close the cause</a>
+              <form v-if="!$store.getters.User">
+                <label for="currentDonation" class="grey-text font-weight-light">My donation:</label>
+                <input id="currentDonation" type="number" min="1" class="form-control" />
+                <button class="btn btn-success w-100" type="submit">Make the donation</button>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </transition>
   </div>
 </template>
 
 <script>
+import firebase from "firebase";
+
 export default {
-  name: "app-cause-detail"
+  name: "app-cause-detail",
+  data() {
+    return {
+      cause: {},
+      loading: true
+    };
+  },
+  computed: {
+    getNeeded() {
+      return `$${this.cause?.neededFund.toFixed(2)}`;
+    },
+    getCollected() {
+      return `$${this.cause?.collectedFund.toFixed(2)}`;
+    },
+    getImage() {
+      return this.cause?.image;
+    }
+  },
+  methods:{
+    deleteCause(){
+      firebase
+      .database()
+      .ref()
+      .child("causes")
+      .child(this.$route.params.id)
+      .remove()
+      .then((res) => {
+        console.log(res);
+        this.$router.push({name:"home"});
+      });
+    }
+  },
+  mounted() {
+    firebase
+      .database()
+      .ref()
+      .child("causes")
+      .child(this.$route.params.id)
+      .once("value")
+      .then((snapshot) => {
+        this.cause = snapshot.val();
+        console.log(snapshot.val());
+        this.loading = false;
+      });
+  }
 };
 </script>
 
@@ -63,5 +123,30 @@ export default {
 .detailDiv {
   margin-bottom: 2%;
   margin-top: 5%;
+}
+
+.loadingRing {
+  position: absolute;
+  margin-left: auto;
+  margin-right: auto;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.fadeIn-enter-active,
+.fadeIn-leave-active {
+  transition: opacity 0.5s;
+}
+.fadeIn-enter, .fadeIn-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+
+.fadeOut-enter-active,
+.fadeOut-leave-active {
+  transition: opacity 0.5s;
+}
+.fadeOut-leave, .fadeOut-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
