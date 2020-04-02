ffee4da1be040a171fc96f5845ec11f224fce8c4
Tundzhay Mollov <graculapsone1998@gmail.com>
2020-04-03 00:23:36 +0300

    Update routes.js
    Included store and added guards for these routes:
    - causeCreate
    - causeDetail

--- app/src/routes.js
+++ app/src/routes.js
@@ -3,15 +3,35 @@ import Login from "./components/auth/Login.vue";
 import Register from "./components/auth/Register.vue";
 import CauseCreate from "./components/cause/Create.vue";
 import CauseDetail from "./components/cause/Detail.vue";
-import Profile from './components/auth/Profile.vue'
+import Profile from "./components/auth/Profile.vue";
 import NotFound from "./components/core/NotFound.vue";
 
+import store from "./store/store";
+
 export default [
-  { name:"home", path: "/", component: HomeMain },
-  { name:"login", path: "/login", component: Login },
-  { name:"register", path: "/register", component: Register },
-  { name:"causeCreate" ,path: "/cause/create", component: CauseCreate },
-  { name: "causeDetail",path: "/cause/detail/:id", component: CauseDetail },
-  { name:"profile", path: "/profile", component: Profile },
-  { name:"", path: "*", component: NotFound }
+  { name: "home", path: "/", component: HomeMain },
+  { name: "login", path: "/login", component: Login },
+  { name: "register", path: "/register", component: Register },
+  {
+    name: "causeCreate",
+    path: "/cause/create",
+    component: CauseCreate,
+    beforeEnter: (to, from, next) => {
+      if (to.name == "causeCreate" && !store.getters.User) {
+        next({ name: "login" });
+      } else next();
+    }
+  },
+  {
+    name: "causeDetail",
+    path: "/cause/detail/:id",
+    component: CauseDetail,
+    beforeEnter: (to, from, next) => {
+      if (to.name == "causeDetail" && to.params.id && !store.getters.User) {
+        next({ name: "login" });
+      } else next();
+    }
+  },
+  { name: "profile", path: "/profile", component: Profile },
+  { name: "", path: "*", component: NotFound }
 ];
