db236ed0190ab5e5e0d0f591436d67b6f882b40e
Tundzhay Mollov <graculapsone1998@gmail.com>
2020-04-03 00:40:39 +0300

    Create Cause/NoCause.vue
    Component to show if there is no causes @ dashboard

---
+++ app/src/components/cause/NoCauses.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1 class="text-center">NO CAUSES :(</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app-no-causes"
+};
+</script>
+
+<style scoped>
+</style>
