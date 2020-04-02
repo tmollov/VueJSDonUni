05284a2d026a8d06f2e9aa1cdd93b5a8f0fa0702
Tundzhay Mollov <graculapsone1998@gmail.com>
2020-04-03 00:02:17 +0300

    Update routes.js
    Added:
    - route for Profile
    - named routes
    - dymanic routes

--- app/src/routes.js
+++ app/src/routes.js
@@ -1,17 +1,17 @@
 import HomeMain from "./components/home/Main.vue";
-import HomeDashbord from "./components/home/Dashboard.vue";
 import Login from "./components/auth/Login.vue";
 import Register from "./components/auth/Register.vue";
 import CauseCreate from "./components/cause/Create.vue";
 import CauseDetail from "./components/cause/Detail.vue";
+import Profile from './components/auth/Profile.vue'
 import NotFound from "./components/core/NotFound.vue";
 
 export default [
-  { path: "/", component: HomeMain },
-  { path: "/dashboard", component: HomeDashbord },
-  { path: "/login", component: Login },
-  { path: "/register", component: Register },
-  { path: "/cause/create", component: CauseCreate },
-  { path: "/cause/detail", component: CauseDetail },
-  { path: "*", component: NotFound }
+  { name:"home", path: "/", component: HomeMain },
+  { name:"login", path: "/login", component: Login },
+  { name:"register", path: "/register", component: Register },
+  { name:"causeCreate" ,path: "/cause/create", component: CauseCreate },
+  { name: "causeDetail",path: "/cause/detail/:id", component: CauseDetail },
+  { name:"profile", path: "/profile", component: Profile },
+  { name:"", path: "*", component: NotFound }
 ];
