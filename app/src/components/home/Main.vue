42d380a30c8c74d589b8ea13c23a808b49f1d55c
Tundzhay Mollov <graculapsone1998@gmail.com>
2020-04-03 00:42:51 +0300

    Update Home/Main.vue
    Added causes in created method

--- app/src/components/home/Main.vue
+++ app/src/components/home/Main.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
     <app-home-guest v-if="!loggedIn"></app-home-guest>
-    <app-home-dashboard v-if="loggedIn"></app-home-dashboard>
+    <app-home-dashboard v-if="loggedIn" :causes="causeArr"></app-home-dashboard>
   </div>
 </template>
 
 <script>
 import AppHomeGuest from "./Guest";
 import AppHomeDashboard from "./Dashboard";
+
+import firebase from "firebase";
 
 export default {
   name: "app-home",
@@ -18,7 +20,29 @@ export default {
   computed: {
     loggedIn() {
       return this.$store.getters.User;
+    },
+    causeArr() {
+      return this.causes;
     }
+  },
+  data() {
+    return {
+      causes: null
+    };
+  },
+  created() {
+    firebase
+      .database()
+      .ref()
+      .once("value")
+      .then((snapshot) => {
+        if (snapshot.val() == null) {
+           this.causes = [];
+           return;
+        }
+        
+        this.causes = snapshot.val().causes;
+      });
   }
 };
 </script>
