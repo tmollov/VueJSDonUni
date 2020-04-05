560fb6b8aa7484b6939c245f5147aafd58e62e5f
Tundzhay Mollov <graculapsone1998@gmail.com>
2020-04-05 13:37:02 +0300

    Create UserInfo.js

---
+++ app/src/models/UserInfo.js
@@ -0,0 +1,10 @@
+export class UserInfo {
+  constructor(firstName, lastName, phone, profileImage, about, email) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phone = phone;
+    this.profileImage = profileImage;
+    this.about = about;
+    this.email = email;
+  }
+}
