70ef9531972d482dc44a3ec8a969b4f3789c39f1
Tundzhay Mollov <graculapsone1998@gmail.com>
2020-04-05 13:37:58 +0300

    Update routes.js
    Added Profile/Welcome component and new guards

--- app/src/routes.js
+++ app/src/routes.js
@@ -3,15 +3,44 @@ import Login from "./components/auth/Login.vue";
 import Register from "./components/auth/Register.vue";
 import CauseCreate from "./components/cause/Create.vue";
 import CauseDetail from "./components/cause/Detail.vue";
-import Profile from "./components/auth/Profile.vue";
+import Profile from "./components/profile/Profile.vue";
+import WelcomeNewUser from './components/profile/Welcome.vue'
 import NotFound from "./components/core/NotFound.vue";
 
 import store from "./store/store";
 
 export default [
   { name: "home", path: "/", component: HomeMain },
-  { name: "login", path: "/login", component: Login },
-  { name: "register", path: "/register", component: Register },
+  {
+    name: "login",
+    path: "/login",
+    component: Login,
+    beforeEnter: (to, from, next) => {
+      if (to.name == "login" && store.getters.User) {
+        next({ name: "home" });
+      } else next();
+    }
+  },
+  {
+    name: "register",
+    path: "/register",
+    component: Register,
+    beforeEnter: (to, from, next) => {
+      if (to.name == "register" && store.getters.User) {
+        next({ name: "home" });
+      } else next();
+    }
+  },
+  {
+    name: "welcome",
+    path: "/welcome",
+    component: WelcomeNewUser,
+    beforeEnter: (to, from, next) => {
+      if (to.name == "welcome" && !store.getters.User) {
+        next({ name: "login" });
+      } else next();
+    }
+  },
   {
     name: "causeCreate",
     path: "/cause/create",
@@ -32,6 +61,15 @@ export default [
       } else next();
     }
   },
-  { name: "profile", path: "/profile", component: Profile },
+  {
+    name: "profile",
+    path: "/profile",
+    component: Profile,
+    beforeEnter: (to, from, next) => {
+      if (to.name == "profile" && !store.getters.User) {
+        next({ name: "login" });
+      } else next();
+    }
+  },
   { name: "", path: "*", component: NotFound }
 ];
