7716e291216d1daee172e991c048e4f32e85a809
Tundzhay Mollov <graculapsone1998@gmail.com>
2020-04-10 21:18:54 +0300

    Update MyCauses.vue

--- app/src/components/profile/My/MyCauses.vue
+++ app/src/components/profile/My/MyCauses.vue
@@ -2,18 +2,19 @@
   <div class="container mb-lg-5">
     <h1 class="text-center border-bottom border-dark">My Causes</h1>
     <div class="mt-2 row mx-auto">
+      <template v-if="IsThereCauses">
+        <figure class="imghvr-shutter-out-diag-1" v-for="(cause,index) in Causes" :key="index">
+          <img :src="cause.image" />
+          <figcaption>
+            {{cause.title}}
+            <router-link :to="LinkToDetails(cause.id)"></router-link>
+          </figcaption>
+        </figure>
+      </template>
 
-      <div class="text-center w-100 mt-5" v-if="!IsThereCause">
-        <h5 >You dont have a cause yet.</h5>
+      <div v-else class="text-center w-100 mt-5" >
+        <h5>You do not have any causes yet.</h5>
       </div>
-
-      <figure class="imghvr-shutter-out-diag-1" v-for="(cause,index) in Causes" :key="index">
-        <img :src="cause.image" />
-        <figcaption>
-          {{cause.title}}
-          <router-link :to="{ name: 'causeDetail', params: { id: cause.id }}"></router-link>
-        </figcaption>
-      </figure>
     </div>
   </div>
 </template>
@@ -21,10 +22,24 @@
 <script>
 export default {
   name: "app-my-causes",
+  data() {
+    return {
+      IsThereCauses: true
+    };
+  },
   props: { Causes: Array },
-  computed:{
-    IsThereCause(){
-      return this.Causes.length > 0;
+  watch: {
+    Causes(newVal, oldVal) {
+      if (newVal.length == 0) {
+        this.IsThereCauses = false;
+      } else {
+        this.IsThereCauses = true;
+      }
+    }
+  },
+  methods: {
+    LinkToDetails(id) {
+      return { name: "causeDetail", params: { id: id } };
     }
   }
 };
