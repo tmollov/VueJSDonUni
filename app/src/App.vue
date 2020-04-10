ed3f0b79c048b1861c265d4a24df98556e97527d
Tundzhay Mollov <graculapsone1998@gmail.com>
2020-04-10 14:03:03 +0300

    Update App.vue
    Added global styles for transitions and loading ring

--- app/src/App.vue
+++ app/src/App.vue
@@ -8,7 +8,7 @@
 
 <script>
 import AppFooter from "./components/core/Footer";
-import AppNavigation from './components/core/Navigation';
+import AppNavigation from "./components/core/Navigation";
 
 export default {
   name: "App",
@@ -20,4 +20,28 @@ export default {
 </script>
 
 <style>
+.loadingRing {
+  position: absolute;
+  margin-left: auto;
+  margin-right: auto;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.fadeIn-enter-active,
+.fadeIn-leave-active {
+  transition: opacity 0.5s;
+}
+.fadeIn-enter, .fadeIn-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+
+.fadeOut-enter-active,
+.fadeOut-leave-active {
+  transition: opacity 0.5s;
+}
+.fadeOut-leave, .fadeOut-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
 </style>
