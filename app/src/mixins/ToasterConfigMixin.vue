229a4d6b26362496805bcc399c16ac5a940be48b
Tundzhay Mollov <graculapsone1998@gmail.com>
2020-04-10 21:16:40 +0300

    Create ToasterConfigMixin.vue

---
+++ app/src/mixins/ToasterConfigMixin.vue
@@ -0,0 +1,72 @@
+<script>
+export default {
+  data() {
+    return {
+      toastType: {
+        success: "success",
+        error: "error",
+        warning: "warning",
+        info: "info",
+        default: "default"
+      },
+      toastDuration: {
+        oneSec: 1000,
+        twoSec: 2000,
+        threeSec: 3000,
+        fiveSec: 5000,
+        tenSec: 10000
+      },
+      toastPosition: {
+        bottom: "bottom",
+        bottomRight: "bottom-right",
+        bottomLeft: "bottom-left",
+        top: "top",
+        topRight: "top-right",
+        topLeft: "top-left"
+      },
+      toastDismissible: {
+        On: true,
+        Off: false
+      },
+      toastQueue: {
+        On: true,
+        Off: false
+      }
+    };
+  },
+  methods: {
+    toastError(message) {
+      this.$toast.open({
+        message: message,
+        type: this.toastType.error,
+        dismissible: this.toastDismissible.On,
+        duration: this.toastDuration.fiveSec
+      });
+    },
+    toastSuccess(message) {
+      this.$toast.open({
+        message: message,
+        type: this.toastType.success,
+        dismissible: this.toastDismissible.On,
+        duration: this.toastDuration.threeSec
+      });
+    },
+    toastInfo(message) {
+      this.$toast.open({
+        message: message,
+        type: this.toastType.info,
+        dismissible: this.toastDismissible.On,
+        duration: this.toastDuration.threeSec
+      });
+    },
+    toastWarn(message) {
+      this.$toast.open({
+        message: message,
+        type: this.toastType.warning,
+        dismissible: this.toastDismissible.On,
+        duration: this.toastDuration.threeSec
+      });
+    }
+  }
+};
+</script>
