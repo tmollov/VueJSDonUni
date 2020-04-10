e139490cc7e563a765a0799d6a88a678347e442e
Tundzhay Mollov <graculapsone1998@gmail.com>
2020-04-10 16:52:23 +0300

    Updated Profile/My/MyCauses.vue
    Added div in case of no causes

--- app/src/components/profile/My/MyCauses.vue
+++ app/src/components/profile/My/MyCauses.vue
@@ -2,6 +2,11 @@
   <div class="container mb-lg-5">
     <h1 class="text-center border-bottom border-dark">My Causes</h1>
     <div class="mt-2 row mx-auto">
+
+      <div class="text-center w-100 mt-5" v-if="!IsThereCause">
+        <h5 >You dont have a cause yet.</h5>
+      </div>
+
       <figure class="imghvr-shutter-out-diag-1" v-for="(cause,index) in Causes" :key="index">
         <img :src="cause.image" />
         <figcaption>
@@ -16,7 +21,12 @@
 <script>
 export default {
   name: "app-my-causes",
-  props: { Causes: Array }
+  props: { Causes: Array },
+  computed:{
+    IsThereCause(){
+      return this.Causes.length > 0;
+    }
+  }
 };
 </script>
 
