69846f93a3e155ae997699d63f4d29d2932f2c6b
Tundzhay Mollov <graculapsone1998@gmail.com>
2020-04-10 15:23:13 +0300

    Update routes.js
    Removed Welcome route

--- app/src/routes.js
+++ app/src/routes.js
@@ -3,10 +3,9 @@ import Login from "./components/auth/Login.vue";
 import Register from "./components/auth/Register.vue";
 import CauseCreate from "./components/cause/Create.vue";
 import CauseDetail from "./components/cause/Detail.vue";
-import CauseEdit from './components/cause/Edit.vue';
+import CauseEdit from "./components/cause/Edit.vue";
 import Profile from "./components/profile/Profile.vue";
-import ProfileEdit from "./components/profile/Edit.vue"
-import WelcomeNewUser from './components/profile/Welcome.vue'
+import ProfileEdit from "./components/profile/Edit.vue";
 import NotFound from "./components/core/NotFound.vue";
 
 import store from "./store/store";
@@ -21,7 +20,7 @@ export default [
       if (to.name == "login" && store.getters.User) {
         next({ name: "home" });
       } else next();
-    }
+    },
   },
   {
     name: "register",
@@ -31,57 +30,7 @@ export default [
       if (to.name == "register" && store.getters.User) {
         next({ name: "home" });
       } else next();
-    }
-  },
-  {
-    name: "welcome",
-    path: "/profile/welcome",
-    component: WelcomeNewUser,
-    beforeEnter: (to, from, next) => {
-      if (to.name == "welcome" && !store.getters.User) {
-        next({ name: "login" });
-      } else next();
-    }
-  },
-  {
-    name: "profileEdit",
-    path: "/profile/edit",
-    component: ProfileEdit,
-    beforeEnter: (to, from, next) => {
-      if (to.name == "profileEdit" && !store.getters.User) {
-        next({ name: "login" });
-      } else next();
-    }
-  },
-  {
-    name: "causeCreate",
-    path: "/cause/create",
-    component: CauseCreate,
-    beforeEnter: (to, from, next) => {
-      if (to.name == "causeCreate" && !store.getters.User) {
-        next({ name: "login" });
-      } else next();
-    }
-  },
-  {
-    name: "causeDetail",
-    path: "/cause/detail/:id",
-    component: CauseDetail,
-    beforeEnter: (to, from, next) => {
-      if (to.name == "causeDetail" && to.params.id && !store.getters.User) {
-        next({ name: "login" });
-      } else next();
-    }
-  },
-  {
-    name: "causeEdit",
-    path: "/cause/edit/:id",
-    component: CauseEdit,
-    beforeEnter: (to, from, next) => {
-      if (to.name == "causeEdit" && to.params.id && !store.getters.User) {
-        next({ name: "login" });
-      } else next();
-    }
+    },
   },
   {
     name: "profile",
@@ -91,7 +40,47 @@ export default [
       if (to.name == "profile" && !store.getters.User) {
         next({ name: "login" });
       } else next();
-    }
+    },
   },
-  { name: "", path: "*", component: NotFound }
+  {
+    name: "profileEdit",
+    path: "/profile/edit",
+    component: ProfileEdit,
+    beforeEnter: (to, from, next) => {
+      if (to.name == "profileEdit" && !store.getters.User) {
+        next({ name: "login" });
+      } else next();
+    },
+  },
+  {
+    name: "causeCreate",
+    path: "/cause/create",
+    component: CauseCreate,
+    beforeEnter: (to, from, next) => {
+      if (to.name == "causeCreate" && !store.getters.User) {
+        next({ name: "login" });
+      } else next();
+    },
+  },
+  {
+    name: "causeDetail",
+    path: "/cause/detail/:id",
+    component: CauseDetail,
+    beforeEnter: (to, from, next) => {
+      if (to.name == "causeDetail" && to.params.id && !store.getters.User) {
+        next({ name: "login" });
+      } else next();
+    },
+  },
+  {
+    name: "causeEdit",
+    path: "/cause/edit/:id",
+    component: CauseEdit,
+    beforeEnter: (to, from, next) => {
+      if (to.name == "causeEdit" && to.params.id && !store.getters.User) {
+        next({ name: "login" });
+      } else next();
+    },
+  },
+  { name: "", path: "*", component: NotFound },
 ];
