1232e11c57f63ef3ecef4ec05a2ad1403eafc802
Tundzhay Mollov <graculapsone1998@gmail.com>
2020-04-10 15:27:47 +0300

    Create HomeMixin.vue

---
+++ app/src/mixins/HomeMixin.vue
@@ -0,0 +1,26 @@
+<script>
+import firebase from "firebase"
+export default {
+  data() {
+    return {
+      causes: null
+    };
+  },
+  methods: {
+    LoadCauses() {
+      firebase
+        .database()
+        .ref()
+        .child("causes")
+        .once("value")
+        .then(response => {
+          if (response.val() == null) {
+            this.causes = [];
+            return;
+          }
+          this.causes = response.val();
+        });
+    }
+  }
+};
+</script>
